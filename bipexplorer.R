a1a13f60731e622a96e3ee956c103edd790598f0
kmgriff <griffin.ngu@gmail.com>
2020-10-15 16:23:38 -0400

    Add files via upload

---
+++ bipexplorer.R
@@ -0,0 +1,348 @@
+##########################################################
+# bipexplorer.R
+# Imports MLB Statcast data and precomputed regression objects from local storage, slices
+# them into useful subsets, then defines an RShiny server to create interactive plots.
+#
+# The full data plot generates wOBABIP heatmaps over all of the data between 2017-2020.
+# Interactive sliders can be used to limit the data observed.  Tip: Try setting it to
+# "Launch speed", and limit the plot to 0-50 MPH.  Do players tend to bunt toward first base,
+# or third base?
+#
+# The Team Explorer shows the plot for specific teams, and can be further subsetted by
+# year.  If they are the hitting team, wOBABIP shows how they perform, while in fielding mode
+# it shows every other team's performance against them.  Checking "Difference plot" will show
+# how their offense / defense compares against the total dataset.  Checking "Away games only"
+# reduces the impact of different stadium dimensions.
+#
+# TODO: Checking "Difference" will show how their offense / defense compares against the
+# average of all the teams' regressions for the same subsets.
+##########################################################
+
+library(tidyverse)
+library(np)
+library(sp)
+library(metR)
+library(geometry)
+library(shiny)
+library(shinythemes)
+library(RColorBrewer)
+library(plotly)
+
+totaldat <- readRDS("all_mlb.RDS")
+totalwobareg <- readRDS("gridwobafull.RDS")
+totalwobareg2d <-
+  lapply(as.character(2017:2020), function(x) {
+    readRDS(sprintf("gridwobafull2d_%s.RDS", x))
+  })
+names(totalwobareg2d) <- 2017:2020
+totaldensreg <- readRDS("griddensfull.RDS")
+totaldensreg2d <-
+  lapply(as.character(2017:2020), function(x) {
+    readRDS(sprintf("griddensfull2d_%s.RDS", x))
+  })
+names(totaldensreg2d) <- 2017:2020
+totalgrid <- readRDS("gridfull.RDS")
+totalgrid2d <-
+  lapply(as.character(2017:2020), function(x) {
+    readRDS(sprintf("gridfull2d_%s.RDS", x))
+  })
+names(totalgrid2d) <- 2017:2020
+
+
+
+totalwobadens <-
+  totalgrid %>% mutate(wOBABIP = totalwobareg$mean, density = totaldensreg$dens)
+totalwobadens2d <-
+  lapply(as.character(2017:2020), function(x) {
+    data.frame(get(x, totalgrid2d)) %>% mutate(
+      wOBABIP = get(x, totalwobareg2d)$mean,
+      density = get(x, totaldensreg2d)$dens
+    )
+  })
+names(totalwobadens2d) <- 2017:2020
+
+calcTeamWoba <- function (team, awayOnly, year, fieldPosition) {
+  subset <- totaldat %>%
+    filter(lubridate::year(game_date) == year)
+  
+  if (fieldPosition == "hitting") {
+    subset <- subset %>% filter(hitting_team == team)
+  } else {
+    subset <- subset %>% filter(fielding_team == team)
+  }
+  
+  if (awayOnly) {
+    subset <- subset %>% filter(away_team == team)
+  }
+  
+  txdat <-
+    data.frame(x = subset$launch_angle,
+               y = subset$launch_speed)
+  
+  totalgrid <- get(as.character(year), totalgrid2d)
+  
+  hull <- convhulln(as.matrix(txdat))
+  grid_inhull <-
+    totalgrid[inhulln(hull, as.matrix(totalgrid)), ]
+  
+  reg <- npreg(
+    bws = c(5, 40),
+    bwtype = "adaptive_nn",
+    exdat = grid_inhull,
+    txdat = txdat,
+    tydat = as.numeric(subset$woba_value)
+  )
+  
+  density <- grid_inhull %>%
+    map(~ {
+      attributes(.x) <- NULL
+      .x
+    }) %>%
+    as.data.frame() %>%
+    npudens(
+      bws = c(5, 40),
+      bwtype = "adaptive_nn",
+      edat = .,
+      tdat = txdat
+    )
+  
+  out <-
+    grid_inhull %>% mutate(wOBABIP = reg$mean, density = density$dens)
+  
+  return(list(out, length(subset[[1]])))
+}
+
+
+plotWobaMargin <- function (dat, n) {
+  breaks <- seq(0, 2, length.out = 9)
+  dims <- names(dat)[1:2] %>%
+    map(~case_when(
+      .x == "launch_angle" ~ "Vertical angle",
+      .x == "launch_speed" ~ "Launch speed",
+      .x == "spray_angle" ~ "Horizontal angle"
+      ))
+  
+  p <- ggplot(dat %>% 
+                rename_all(~c("x", "y", "z")),
+              aes(x = x, y = y, z = z)) +
+      geom_contour_filled(breaks = breaks) +
+      geom_contour_tanaka(breaks = breaks) +
+      scale_fill_manual(values = brewer.pal(9, "Greens"), drop = FALSE)
+    
+    if (!"Vertical angle" %in% dims) {
+      p <- p + coord_polar(theta = "y",
+                           start = pi / 2,
+                           direction = -1) +
+        scale_y_continuous(
+          breaks = seq(45, 135, by = 10),
+          minor_breaks = NULL,
+          limits = c(-180, 180)
+        ) +
+        scale_x_continuous(breaks = seq(0, 120, by = 10),
+                           limits = c(0, 120))
+    }
+    else if (!"Horizontal angle" %in% dims) {
+      p <- p + coord_polar(theta = "y",
+                           start = pi / 2,
+                           direction = -1) +
+        scale_y_continuous(
+          breaks = seq(-90, 90, by = 10),
+          minor_breaks = NULL,
+          limits = c(-180, 180)
+        ) +
+        scale_x_continuous(breaks = seq(0, 120, by = 10),
+                           limits = c(0, 120))
+    }
+    else {
+      p <- p + coord_cartesian() +
+        scale_y_continuous(
+          breaks = seq(-90, 90, by = 10),
+          minor_breaks = NULL,
+          limits = c(-90, 90)
+        ) +
+        scale_x_continuous(breaks = seq(45, 135, by = 10),
+                           limits = c(45, 135))
+    }
+    p + labs(
+      x = dims[1],
+      y = dims[2],
+      fill = "wOBABIP",
+      title = "wOBABIP",
+      caption = paste0("N = ", n)
+    )
+}
+
+plotWobaTeam <- function (dat, difference, n, year) {
+  if (difference) {
+    wobadens <- get(as.character(year), totalwobadens2d)
+    
+    dat <- dat %>%
+      inner_join(
+        wobadens,
+        by = c("launch_angle", "launch_speed"),
+        suffix = c("x", "y")
+      ) %>%
+      mutate(wOBABIP = wOBABIPx - wOBABIPy,
+             density = densityx - densityy)
+    
+    
+    colorpal = brewer.pal(11, "PiYG")
+    breaks = seq(-.5, .5, length.out = 11)
+  } else {
+    colorpal = brewer.pal(9, "Greens")
+    breaks <- seq(0, 2, length.out = 9)
+  }
+  
+  ggplot(dat, aes(x = launch_speed, y = launch_angle, z = wOBABIP)) +
+      geom_contour_filled(breaks = breaks) +
+      geom_contour_tanaka(breaks = breaks) +
+      scale_fill_manual(values = colorpal, drop = FALSE) +
+      coord_polar(theta = "y",
+                  start = pi / 2,
+                  direction = -1) +
+      scale_y_continuous(
+        breaks = seq(-90, 90, by = 10),
+        minor_breaks = NULL,
+        limits = c(-180, 180)
+      ) +
+      scale_x_continuous(breaks = seq(0, 120, by = 10),
+                         limits = c(0, 120)) +
+      labs(
+        x = "Launch speed",
+        y = "Launch angle",
+        fill = "wOBABIP",
+        title = "wOBABIP",
+        caption = paste0("N = ", n)
+      )
+}
+
+# Define UI
+ui <- fluidPage(theme = shinytheme("lumen"),
+                navbarPage(
+                  "BIP WOBA Viewer",
+                  tabPanel("Full dataset",
+                           sidebarLayout(
+                             sidebarPanel(
+                               # Select dimension to flatten
+                               selectInput(
+                                 inputId = "dim",
+                                 label = strong("Select dimension for windowed average"),
+                                 choices = c(
+                                   "Vertical angle" = "launch_angle",
+                                   "Horizontal angle" = "spray_angle",
+                                   "Launch speed" = "launch_speed"
+                                 )
+                               ),
+                               
+                               
+                               # Select range for marginal average
+                               sliderInput(
+                                 inputId = "range",
+                                 label = strong("Select range for windowed average"),
+                                 min = -180,
+                                 max = 180,
+                                 value = c(90, 90)
+                               ),
+                             ),
+                             
+                             mainPanel(plotOutput(outputId = "wobaplotfull", height = "800px"))
+                           ),),
+                  tabPanel("Team Explorer",
+                           sidebarLayout(
+                             sidebarPanel(
+                               selectInput(
+                                 inputId = "year",
+                                 label = "Year",
+                                 choices = 2017:2020
+                               ),
+                               selectInput(
+                                 inputId = "team",
+                                 label = "Team",
+                                 choices = levels(as.factor(totaldat$away_team))
+                               ),
+                               selectInput(
+                                 inputId = "fieldposition",
+                                 label = "Field position",
+                                 choices = c("Hitting" = "hitting",
+                                             "Fielding" = "fielding")
+                               ),
+                               checkboxInput(inputId = "awayonly",
+                                             label = "Away games only"),
+                               checkboxInput(inputId = "difference",
+                                             label = "Difference plot")
+                             ),
+                             
+                             mainPanel(plotOutput(outputId = "wobaplotteam", height = "800px"))
+                           ))
+                ))
+
+server <- function(input, output, session) {
+  dims <- reactive({
+    req(input$dim)
+    setdiff(c("launch_speed", "spray_angle", "launch_angle"),
+            c(input$dim))
+  })
+  
+  dimRange <- reactive({
+    req(input$dim)
+    range(totalwobadens[[input$dim]])
+  })
+  
+  # Subset and average data
+  wobamarginalized <- reactive({
+    req(input$dim, input$range)
+    totalwobadens %>%
+      filter_at(input$dim, ~.x >= input$range[1] & .x <= input$range[2]) %>%
+      group_by_at(dims()) %>%
+      summarize(wOBABIP = weighted.mean(wOBABIP, density))
+  })
+  
+  wobateam <- reactive({
+    req(input$year,
+        input$team,
+        input$fieldposition)
+    calcTeamWoba(input$team,
+                 input$awayonly,
+                 input$year,
+                 input$fieldposition)
+  })
+  
+  observe({
+    req(input$dim)
+    updateSliderInput(
+      session,
+      "range",
+      min = dimRange()[1],
+      max = dimRange()[2],
+      value = dimRange
+    )
+  })
+  
+  output$wobaplotfull <- renderPlot({
+    plotWobaMargin(
+      wobamarginalized(),
+      length(totaldat[[1]])
+    )
+  })
+  
+  output$wobaplotteam <- renderPlot({
+    plotWobaTeam(wobateam()[[1]],
+                 input$difference,
+                 wobateam()[[2]],
+                 input$year)
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+# all2d <- lapply(levels(as.factor(totaldat$fielding_team)), FUN = function(team) {calcTeamWoba(team, FALSE, 2017, "fielding")})
+# 
+# all2ddelist <- lapply(all2d, FUN = function(x) {x[[1]]})
+# 
+# all2davg <- all2ddelist %>%
+#   bind_rows() %>%
+#   group_by(launch_speed, launch_angle) %>%
+#   summarize(wOBABIP = weighted.mean(wOBABIP, density), density = weighted.mean(density, density))
+# 
+# plotWobaTeam(all2davg, TRUE, 0, 2017)
+# plotWobaTeam(get("2017", totalwobadens2d), TRUE, 0, 2017)
---
+++ wobabip.R
@@ -0,0 +1,291 @@
+##########################################################
+# wOBABIP.R
+# This script is used for initially pulling and updating raw data, and for precomputing
+# large regression objects.  Unless these files need to be updated, you can proceed directly
+# to bipexplorer.R to generate plots.
+#
+# Performs a nonparametric kernel regression on MLB Statcast data with launch angle, launch
+# speed, and spray angle as the random variables, with wOBA (weighted on-base average) as the
+# outcome variable.  Statcast provides data going back to about 2017.
+#
+# Bandwidth calculations for the kernel regression and unconditional density estimator, as
+# well as the regression and density calculation themselves, all run in O(n^2) time.  The
+# bandwidths are calculated on a small random sample of the data, n = 3000, while the
+# regression is performed on a grid of specified size corresponding to the density of actual
+# data points.  This approach has been roughly approved by the author of the 'np' package as
+# statistically sound.  The correct bandwidths computed for this data are roughly 5 for
+# vertical launch angle, 50 for launch speed, and 500 for horizontal launch angle.
+##########################################################
+
+
+library(tidyverse)
+library(baseballr)
+library(np)
+library(sp)
+library(geometry)
+
+ftime <- function(f) {
+  f <- as_mapper(f)
+  function(...) {
+    pre.time <- proc.time()
+    x <- f(...)
+    print(proc.time() - pre.time)
+    x
+  }
+}
+
+ftime(function() for(i in 1:(10**8)) {x <- 1 + 1})()
+
+# Collect Statcast data
+
+# Pull initial Statcast data in chunks of 3 days, store in current working directory as "all_mlb.RDS"
+if (!"all_mlb.RDS" %in% dir()) {
+  rawdat <- data.frame()
+  for (dates in seq(lubridate::as_date("2017-01-01"), Sys.Date(), 3)) {
+    tdat <- scrape_statcast_savant(dates, dates + 2) %>%
+      mutate_at("game_type", as.character)
+    if (length(tdat[[1]]) != 0) {
+      rawdat <- bind_rows(rawdat, tdat)
+    }
+  }
+  
+  # Read data from "all_mlb.RDS", remove last day of data, then update
+} else {
+  rawdat <- readRDS("all_mlb.RDS") %>%
+    filter(game_date < Sys.Date() - 1)
+  for (dates in seq(max(rawdat$game_date), Sys.Date(), by = "days")) {
+    tdat <-
+      lubridate::as_date(dates) %>%
+      scrape_statcast_savant(., .) %>%
+      mutate_at("game_type", as.character)
+    if (length(tdat[[1]]) != 0) {
+      rawdat <- bind_rows(rawdat, tdat)
+    }
+  }
+}
+rm(tdat, dates)
+rawdat <- rawdat %>%
+  filter(type == "X") %>%
+  filter_at(vars("launch_speed", "launch_angle", "hc_x", "hc_y"),
+            ~ !is.na(.x))
+
+
+rawdat <- rawdat %>%
+  mutate(spray_angle = (-atan((hc_x - 130) / (213 - hc_y)) + pi / 2) / pi * 180) %>%
+  mutate(spray_angle = 180 - spray_angle) %>%
+  filter_at(vars("spray_angle", "woba_value"), ~ !is.na(.x) &
+              !is.nan(.x)) %>%
+  mutate(fielding_team =
+           case_when(
+             inning_topbot == "Top" ~ home_team,
+             inning_topbot == "Bot" ~ away_team
+           )) %>%
+  mutate(hitting_team =
+           case_when(
+             inning_topbot == "Top" ~ away_team,
+             inning_topbot == "Bot" ~ home_team
+           ))
+
+saveRDS(rawdat, "all_mlb.RDS")
+
+# Used for exploration only
+calcWobaBandwidths <- ftime(function(n) {
+  print(paste0("Calculating wOBA Bandwidths 3D: ", n))
+  rawdat %>%
+    slice_sample(n = n) %>%
+    {
+      npregbw(
+        xdat = data.frame(
+          x = .$launch_angle,
+          y = .$launch_speed,
+          z = .$spray_angle
+        ),
+        ydat = as.numeric(.$woba_value),
+        bws = c(5, 50, min(n - 1, 500)),
+        nmulti = 1,
+        bwtype = "generalized_nn",
+        bwmethod = "cv.aic"
+      )
+    }
+})
+
+# Used for exploration only
+calcWobaBandwidths2d <- ftime(function(n) {
+  print(paste0("Calculating wOBA Bandwidths 2D: ", n))
+  rawdat %>%
+    slice_sample(n = n) %>%
+    {
+      npregbw(
+        xdat = data.frame(x = .$launch_angle, y = .$launch_speed),
+        ydat = as.numeric(.$woba_value),
+        # bws = c(5, 50),
+        # nmulti = 1,
+        bwtype = "adaptive_nn",
+        bwmethod = "cv.aic"
+      )
+    }
+})
+
+# Used for exploration only
+calcDensityBandwidths <- ftime(function(n) {
+  print(paste0("Calculating Density Bandwidths 3D: ", n))
+  rawdat %>%
+    slice_sample(n = n) %>%
+    {
+      npudensbw(
+        dat = data.frame(
+          x = .$launch_angle,
+          y = .$launch_speed,
+          z = .$spray_angle
+        ),
+        bws = c(5, 50, min(n - 1, 500)),
+        nmulti = 1,
+        bwtype = "generalized_nn",
+        bwmethod = "cv.ml"
+      )
+    }
+})
+
+# Used for exploration only
+calcDensityBandwidths2d <- ftime(function(n) {
+  print(paste0("Calculating Density Bandwidths 2D: ", n))
+  rawdat %>%
+    slice_sample(n = n) %>%
+    {
+      npudensbw(
+        dat = data.frame(x = .$launch_angle, y = .$launch_speed),
+        bws = c(5, 50),
+        nmulti = 1,
+        bwtype = "generalized_nn",
+        bwmethod = "cv.ml"
+      )
+    }
+})
+
+# bws2d <- calcWobaBandwidths2d(3000)
+# bws3d <- calcWobaBandwidths(3000)
+# densbws2d <- calcDensityBandwidths2d(3000)
+# densbws3d <- calcDensityBandwidths(3000)
+
+dat <- rawdat
+
+# Parse data into a defined grid
+grid <- expand.grid(
+  launch_angle = seq(-90, 90, 2),
+  launch_speed = seq(20, 140, 2),
+  spray_angle = seq(45, 135, by = 10)
+)
+grid2d <-
+  expand.grid(launch_angle = seq(-90, 90, 2),
+              launch_speed = seq(20, 140, 2))
+
+txdat <-
+  data.frame(
+    x = dat$launch_angle,
+    y = dat$launch_speed,
+    z = dat$spray_angle
+  )
+
+# Remove grid coordinates that fall outside of convex hull
+hull <- convhulln(as.matrix(txdat))
+grid_inhull <- grid[inhulln(hull, as.matrix(grid)),]
+saveRDS(grid_inhull, "gridfull.RDS")
+
+# Calculate mean wOBA at each grid coordinate
+calcGridWobaFull <- ftime(function(grid) {
+  print("Calculating mean wOBA for grid")
+  npreg(
+    bws = c(5, 50, 500),
+    bwtype = "generalized_nn",
+    exdat = grid,
+    txdat = txdat,
+    tydat = as.numeric(dat$woba_value)
+  )
+})
+gridwobafull <- calcGridWobaFull(grid_inhull)
+out <- list(gridwobafull$mean, gridwobafull$ntrain)
+names(out) <- c("mean", "n")
+saveRDS(out, "gridwobafull.RDS")
+
+
+
+# Calculate density of observations at each grid coordinate
+calcGridDensFull <- ftime(function(grid) {
+  print("Calculating density for grid")
+  grid %>%
+    map( ~ {
+      attributes(.x) <- NULL
+      .x
+    }) %>%
+    as.data.frame() %>%
+    npudens(
+      bws = c(5, 40, 500),
+      bwtype = "generalized_nn",
+      edat = .,
+      tdat = txdat
+    )
+})
+griddens <- calcGridDensFull(grid_inhull)
+out <- list(griddens$dens, griddens$ntrain)
+names(out) <- c("dens", "n")
+saveRDS(out, "griddensfull.RDS")
+
+
+
+calcWobaDens2d <- function(year, team, position) {
+  subset <- dat %>%
+    filter(lubridate::year(game_date) == year)
+  if(position == "hitting") {
+    subset <- subset %>% filter(hitting_team == team)
+  } else {
+    subset <- subset %>% filter(fielding_team == team)
+  }
+  
+  txdat2d <- data.frame(x = subset$launch_angle,
+                        y = subset$launch_speed)
+  
+  # Remove grid coordinates that fall outside of convex hull
+  hull2d <- convhulln(as.matrix(txdat2d))
+  grid_inhull2d <- grid2d[inhulln(hull2d, as.matrix(grid2d)),]
+  saveRDS(grid_inhull2d, str_glue("gridfull2d_{team}_{position}_{year}.RDS"))
+  
+  
+  # Calculate mean wOBA at each grid coordinate
+  print(str_glue("Calculating mean wOBA 2D for {team} {position} in {year}"))
+  gridwoba2d <- ftime(function() npreg(
+    bws = c(5, 40),
+    bwtype = "adaptive_nn",
+    exdat = grid_inhull2d,
+    txdat = txdat2d,
+    tydat = as.numeric(subset$woba_value)
+  ))()
+  out <- list(gridwoba2d$mean, gridwoba2d$ntrain)
+  names(out) <- c("mean", "n")
+  saveRDS(out, str_glue("gridwobafull2d_{team}_{position}_{year}.RDS"))
+  
+  # Calculate density of observations at each grid coordinate
+  print(str_glue("Calculating density 2D for {team} {position} in {year}"))
+  griddens <- ftime(function() grid_inhull2d %>%
+                      map( ~ {
+                        attributes(.x) <- NULL
+                        .x
+                      }) %>%
+                      as.data.frame() %>%
+                      npudens(
+                        bws = c(5, 40),
+                        bwtype = "adaptive_nn",
+                        edat = .,
+                        tdat = txdat2d
+                      ))()
+  out <- list(griddens$dens, griddens$ntrain)
+  names(out) <- c("dens", "n")
+  saveRDS(out, str_glue("griddensfull2d_{team}_{position}_{year}.RDS"))
+}
+
+walk(dat %>% distinct(lubridate::year(game_date)) %>% pull(), function(year){
+  walk(dat %>% distinct(hitting_team) %>% pull(), function(team) {
+    walk(c("hitting", "fielding"), function(position) {
+      calcWobaDens2d(year, team, position)
+    })
+  })
+})
